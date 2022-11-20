c75bada31ce620f529272b84781748ab012cd114
noebezerra <noebezerra@hotmail.com>
2022-11-19 22:14:28 -0300

    Efeitos no dom

---
+++ origamid/js-completo-es6/js/modules/accordion.js
@@ -0,0 +1,18 @@
+// Accordion List
+export default function initAccordion() {
+  const accordionList = document.querySelectorAll(
+    '[data-anime="accordion"] dt'
+  );
+  if (accordionList.length) {
+    const activeClass = "ativo";
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
---
+++ origamid/js-completo-es6/js/script.js
@@ -0,0 +1,13 @@
+import iniTabNav from "./modules/tabnav.js";
+import initAccordion from "./modules/accordion.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
+
+iniTabNav();
+initAccordion();
+initScrollSuave();
+initAnimacaoScroll();
+initModal();
+initTooltip();
