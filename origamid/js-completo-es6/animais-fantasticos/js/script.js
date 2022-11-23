d7f314057d9a18540415acff6f6a812b21344c14
noebezerra <noebezerra@hotmail.com>
2022-11-22 21:28:41 -0300

    Dropdown & Menu mobile

--- origamid/js-completo-es6/animais-fantasticos/js/script.js
+++ origamid/js-completo-es6/animais-fantasticos/js/script.js
@@ -4,6 +4,8 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 iniTabNav();
 initAccordion();
@@ -11,3 +13,5 @@ initScrollSuave();
 initAnimacaoScroll();
 initModal();
 initTooltip();
+initDropdownMenu();
+initMenuMobile();
