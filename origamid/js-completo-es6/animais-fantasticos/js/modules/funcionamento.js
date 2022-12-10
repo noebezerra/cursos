517270aef864b85aa72a44ca6a7cc17b20742a04
noebezerra <noebezerra@hotmail.com>
2022-12-10 09:36:47 -0300

    fetch

---
+++ origamid/js-completo-es6/animais-fantasticos/js/modules/funcionamento.js
@@ -0,0 +1,15 @@
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSemana = funcionamento.dataset.horario.split(",").map(Number);
+  const agora = new Date();
+  const diaAgora = agora.getDay();
+  const horaAgora = agora.getHours();
+  if (
+    diasSemana.indexOf(diaAgora) !== -1 &&
+    horaAgora >= horarioSemana[0] &&
+    horaAgora < horarioSemana[1]
+  ) {
+    funcionamento.classList.add("aberto");
+  }
+}
