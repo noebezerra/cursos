6440d042b8061f806e3e96907d4bbf9b7d71a5b9
noebezerra <noebezerra@hotmail.com>
2022-12-17 17:47:04 -0300

    TabNav transformado em classe

--- origamid/js-completo-es6/animais-fantasticos/js/script.js
+++ origamid/js-completo-es6/animais-fantasticos/js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from './modules/scroll-suave.js';
-import iniTabNav from './modules/tabnav.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -9,6 +8,7 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 import Accordion from './modules/accordion.js';
+import TabNav from './modules/tabnav.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
@@ -16,7 +16,11 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-iniTabNav();
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
 
 initAnimacaoScroll();
 initModal();
