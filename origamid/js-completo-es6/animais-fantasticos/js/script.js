647c299c82c0fa87c716383f00deb5bb3653cd4e
noebezerra <noebezerra@hotmail.com>
2022-12-17 16:58:25 -0300

    ScrollSuave transformado em classe

--- origamid/js-completo-es6/animais-fantasticos/js/script.js
+++ origamid/js-completo-es6/animais-fantasticos/js/script.js
@@ -1,6 +1,6 @@
+import ScrollSuave from './modules/scroll-suave.js';
 import iniTabNav from './modules/tabnav.js';
 import initAccordion from './modules/accordion.js';
-import initScrollSuave from './modules/scroll-suave.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -10,9 +10,11 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
 
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
+
 iniTabNav();
 initAccordion();
-initScrollSuave();
 initAnimacaoScroll();
 initModal();
 initTooltip();
