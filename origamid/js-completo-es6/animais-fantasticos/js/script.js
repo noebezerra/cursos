2a67658c04e34be69e6a6658a6a5972ff262b44b
noebezerra <noebezerra@hotmail.com>
2022-12-17 17:26:57 -0300

    Accordion transformado em classe

--- origamid/js-completo-es6/animais-fantasticos/js/script.js
+++ origamid/js-completo-es6/animais-fantasticos/js/script.js
@@ -1,6 +1,5 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import iniTabNav from './modules/tabnav.js';
-import initAccordion from './modules/accordion.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
@@ -9,12 +8,16 @@ import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetch-animais.js';
 import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import Accordion from './modules/accordion.js';
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
 iniTabNav();
-initAccordion();
+
 initAnimacaoScroll();
 initModal();
 initTooltip();
