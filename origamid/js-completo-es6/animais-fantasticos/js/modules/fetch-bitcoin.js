e801603afa477d6bf687cce3ff8db960c34a838c
noebezerra <noebezerra@hotmail.com>
2022-12-24 18:19:09 -0300

    refatorado fetch bitcoin

--- origamid/js-completo-es6/animais-fantasticos/js/modules/fetch-bitcoin.js
+++ origamid/js-completo-es6/animais-fantasticos/js/modules/fetch-bitcoin.js
@@ -1,10 +1,10 @@
-export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
+      const btcPreco = document.querySelector(target);
       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
     })
     .catch((error) => {
