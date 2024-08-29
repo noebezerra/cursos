eefc91cc2edb6ebc899f3b90d435fe2a66dfa4c9
Noé Bezerra <noebezerra@hotmail.com>
2024-08-29 16:09:33 +0000

    add reactive forms with `FormBuild` & `Validators`

--- alura/angular/framework-angular/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ alura/angular/framework-angular/memoteca/src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidator';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,23 +11,50 @@ import { PensamentoService } from '../pensamento.service';
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
+  // pensamento: Pensamento = {
+  //   conteudo: '',
+  //   autoria: '',
+  //   modelo: 'modelo1',
+  // };
 
-  constructor(private router: Router, private service: PensamentoService) {}
+  formulario!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private service: PensamentoService,
+    private formBuilder: FormBuilder
+  ) {}
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarpensamento']);
-    });
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarpensamento']);
+      });
+    }
   }
 
   cancelar() {
     this.router.navigate(['/listarpensamento']);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        ]),
+      ],
+      autoria: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          minusculoValidator,
+        ]),
+      ],
+      modelo: ['modelo1', Validators.compose([Validators.required])],
+    });
+  }
 }
--- alura/angular/framework-angular/memoteca/src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ alura/angular/framework-angular/memoteca/src/app/components/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -9,32 +10,51 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./editar-pensamento.component.css'],
 })
 export class EditarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    id: 0,
-    conteudo: '',
-    autoria: '',
-    modelo: '',
-  };
+  // pensamento: Pensamento = {
+  //   id: 0,
+  //   conteudo: '',
+  //   autoria: '',
+  //   modelo: '',
+  // };
+
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.service.buscarPorId(parseInt(id)).subscribe((pensamento) => {
-        this.pensamento = pensamento;
+        this.formulario = this.formBuilder.group({
+          id: [pensamento.id],
+          conteudo: [
+            pensamento.conteudo,
+            Validators.compose([
+              Validators.required,
+              Validators.pattern(/(.|\s)*\S(.|\s)*/),
+            ]),
+          ],
+          autoria: [
+            pensamento.autoria,
+            Validators.compose([Validators.required, Validators.minLength(3)]),
+          ],
+          modelo: [pensamento.modelo],
+        });
       });
     }
   }
 
   editarPensamento() {
-    this.service.editar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarpensamento']);
-    });
+    if (this.formulario.valid) {
+      this.service.editar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarpensamento']);
+      });
+    }
   }
   cancelar() {
     this.router.navigate(['/listarpensamento']);
