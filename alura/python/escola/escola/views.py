3f8d7670154b4da6549779f3f4104b951cebb570
noebezerra <noebezerra@hotmail.com>
2025-03-17 03:26:45 -0300

    add docs `swagger`

--- alura/python/escola/escola/views.py
+++ alura/python/escola/escola/views.py
@@ -6,6 +6,27 @@ from rest_framework.throttling import UserRateThrottle
 from escola.throttles import MatriculasAnonRateThrottle
 
 class EstudantesViewSet(viewsets.ModelViewSet):
+    """
+    CRUD de Estudantes
+
+    ---
+    ### Ordenação:
+    - **nome**: permite ordenar por nome
+    
+    ### Filtragem:
+    - **nome**: permite buscar por nome
+    - **cpf**: permite buscar por cpf
+
+    ### Métodos permitidos:
+    - **GET**: Retorna a lista de estudantes.
+    - **POST**: Cria um novo estudante.
+    - **PUT**: Atualiza um estudante.
+    - **DELETE**: Deleta um estudante.
+
+    ### Classe de Serialização:
+    - **EstudanteSerializer**: usado para serialização e desserialização de dados.
+    - Se a versão da API for 'v2', será usado o EstudanteSerializerV2.
+    """
     queryset = Estudante.objects.all().order_by('id')
     # serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
@@ -17,12 +38,33 @@ class EstudantesViewSet(viewsets.ModelViewSet):
         return EstudanteSerializer
 
 class CursosViewSet(viewsets.ModelViewSet):
+    """
+    CRUD de Cursos
+
+    ---
+    ### Ordenação:
+    - **nome**: permite ordenar por nome
+    
+    ### Métodos permitidos:
+    - **GET**: Retorna a lista de cursos.
+    - **POST**: Cria um novo curso.
+    - **PUT**: Atualiza um curso.
+    - **DELETE**: Deleta um curso.
+    """
     queryset = Curso.objects.all().order_by('id')
     serializer_class = CursoSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ['descricao']
 
 class MatriculasViewSet(viewsets.ModelViewSet):
+    """
+    Lista todas as matrículas
+
+    ---
+    ### Métodos permitidos:
+    - **GET**: Retorna a lista de matrículas.
+    - **POST**: Cria uma nova matrícula.
+    """
     queryset = Matricula.objects.all().order_by('id')
     serializer_class = MatriculaSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
@@ -31,14 +73,32 @@ class MatriculasViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post']
 
 class ListaMatriculasEstudante(generics.ListAPIView):
+    """
+    Lista todas as matrículas por id do estudante
+
+    ---
+    ### Parâmetros:
+    - id (int): id do estudante
+    """
     def get_queryset(self):
+        if getattr(self, 'swagger_fake_view', False):
+            return Matricula.objects.none()
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by('id')
         return queryset
     
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculasCurso(generics.ListAPIView):
+    """
+    Lista todas as matrículas por id do curso
+
+    ---
+    ### Parâmetros:
+    - id (int): id do curso
+    """
     def get_queryset(self):
+        if getattr(self, 'swagger_fake_view', False):
+            return Matricula.objects.none()
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by('id')
         return queryset
 
--- alura/python/escola/setup/urls.py
+++ alura/python/escola/setup/urls.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from escola.views import EstudantesViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasEstudante, ListaMatriculasCurso
 from rest_framework import routers
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Documentação",
+        default_version='v1',
+        description="Documentação da API Escola",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contac@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudantesViewSet, basename='Estudantes')
@@ -13,5 +27,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('estudante/<int:pk>/matriculas/', ListaMatriculasEstudante.as_view()),
     path('curso/<int:pk>/matriculas/', ListaMatriculasCurso.as_view()),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
