7f183bdf7efbcab57d9ad4e4ca21cc35d0c6fbc7
noebezerra <noebezerra@hotmail.com>
2025-08-02 11:39:52 -0300

    aulas python

---
+++ alura/python/escola/escola/views.py
@@ -0,0 +1,11 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+
+@require_GET
+def estudantes(request):
+    if request.method == 'GET':
+        estudante = {
+            'id':1,
+            'nome': 'lais'
+        }
+        return JsonResponse(estudante)
