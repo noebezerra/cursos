d8d8a6a0fe338369509c9bafe16b33562c29a9ba
noebezerra <noebezerra@hotmail.com>
2026-03-18 00:42:34 -0300

    aula 2

--- alura/java/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ alura/java/screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -38,5 +38,21 @@ public class Principal {
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+        // episodios
+//        for (int i = 0; i < dados.totalTemporadas(); i++) {
+//            List<DadosEpisodio> episodios = temporadas.get(i).episodios();
+//            for (DadosEpisodio episodio : episodios) {
+//                System.out.println(episodio.titulo());
+//            }
+//        }
+
+        // episodios
+        temporadas.forEach(
+                t -> t.episodios().forEach(
+                        e -> System.out.println(e.titulo())
+                )
+        );
+
     }
 }
